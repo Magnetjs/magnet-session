7328213838253ddc08a1ef5535712831f078da46
Chua Kiem Fai <kievechua@gmail.com>
2016-04-13 07:30:41 +0800

    Publish build

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,84 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _base = require('magnet-core/dist/base');
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+var _base2 = _interopRequireDefault(_base);
 
 var _koaRedis = require('koa-redis');
 
 var _koaRedis2 = _interopRequireDefault(_koaRedis);
 
+var _koaConvert = require('koa-convert');
+
+var _koaConvert2 = _interopRequireDefault(_koaConvert);
+
 var _koaGenericSession = require('koa-generic-session');
 
 var _koaGenericSession2 = _interopRequireDefault(_koaGenericSession);
 
-var Session = (function () {
-  function Session(app) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Session = function (_Base) {
+  _inherits(Session, _Base);
+
+  function Session() {
     _classCallCheck(this, Session);
 
-    this.app = app;
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Session).apply(this, arguments));
   }
 
   _createClass(Session, [{
     key: 'setup',
-    value: regeneratorRuntime.mark(function setup() {
-      var options;
-      return regeneratorRuntime.wrap(function setup$(context$2$0) {
-        while (1) switch (context$2$0.prev = context$2$0.next) {
-          case 0:
-            options = _extends({}, this.app.config.session);
+    value: function () {
+      var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+        var options;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                options = _extends({}, this.app.config.session);
 
-            options.store = new _koaRedis2['default'](this.app.config.connections.redis['default']);
+                options.store = new _koaRedis2.default(this.app.config.connections.redis.default);
 
-            this.app.application.keys = options.keys;
+                this.app.application.keys = options.keys;
 
-            this.app.session = (0, _koaGenericSession2['default'])(options);
+                this.app.session = (0, _koaGenericSession2.default)(options);
 
-            this.app.application.use(this.app.session);
+                this.app.application.use((0, _koaConvert2.default)(this.app.session));
 
-          case 5:
-          case 'end':
-            return context$2$0.stop();
-        }
-      }, setup, this);
-    })
+              case 5:
+              case 'end':
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function setup() {
+        return ref.apply(this, arguments);
+      }
+
+      return setup;
+    }()
   }]);
 
   return Session;
-})();
+}(_base2.default);
 
-exports['default'] = Session;
-module.exports = exports['default'];
-//# sourceMappingURL=index.js.map
+exports.default = Session;
